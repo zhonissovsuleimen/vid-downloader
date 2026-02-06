b46b6c2783fa1d81fe0b001260170decc1735279
Suleimen Zhonissov <zhonisov.s@gmail.com>
2026-02-06 23:32:37 +0500

    flake for this project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "vid-downloader-tg devshell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        rustc
+        cargo
+        rustfmt
+        rust-analyzer
+
+        chromium
+      ];
+
+      shellHook = ''
+      '';
+
+      env = {
+        TELOXIDE_TOKEN="8051788028:AAFDWTpwCSNxwB-a8PMqDgozaTNU5x-8pqA";
+        RUST_BACKTRACE="1";
+        OPENSSL_DIR="${pkgs.openssl.dev}";
+        OPENSSL_LIB_DIR="${pkgs.openssl.out}/lib";
+        OPENSSL_INCLUDE_DIR="${pkgs.openssl.dev}/include";
+      };
+    };
+  };
+}
